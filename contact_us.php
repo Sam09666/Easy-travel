d73aeb51969f5e40808aab4feefd960fbf4a14e4
Sam09666 <sanyam0966@gmail.com>
2024-07-05 21:37:02 +0530

    commit msg

---
+++ contact_us.php
@@ -0,0 +1,27 @@
+<?php
+   $conn= mysqli_connect('localhost','root','','tour');  //establishing connection
+    if(!$conn)
+    {
+        echo"Connection Failed:",mysqli_connect_error();
+        exit;
+    }
+    echo"connected"
+    ?>
+    
+    // $name=$_POST['name'];
+    // $email=$_POST['email'];
+    // $phone=$_POST['phone'];
+    // $subject=$_POST['subject'];
+    // $message=$_POST['message'];
+
+    // $sql="Insert into contact(Name,Email,Phone,Subject,Message) values ('$name','$email','$phone','$subject','$message')";
+    // $result=mysqli_query($conn,$sql);
+    // if(!$result)
+    // {
+    //     echo "error: ",mysqli_error($conn);
+    //     exit;
+    // }
+    // echo "We will contact you soon";
+    // mysqli_close($conn);
+ 
+
